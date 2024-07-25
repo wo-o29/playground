7036f8a3fecdcdf335182a2371a60de4aef81d78
wo-o29 <woogur29@gmail.com>
2024-07-25 21:07:33 +0900

    chore: linaria 초기 설정

--- firework/src/pages/index.tsx
+++ firework/src/pages/index.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <>123</>;
+  const [count, setCount] = useState(1);
+  console.log(1);
+
+  useEffect(() => {
+    console.log(2);
+
+    return () => {
+      console.log(3);
+    };
+  }, [count]);
+
+  useEffect(() => {
+    console.log(4);
+
+    setCount((count) => count + 1);
+  }, []);
+
+  return <Child count={count} />;
+}
+
+function Child({ count }: any) {
+  console.log("A");
+  useEffect(() => {
+    console.log(5);
+
+    return () => {
+      console.log(6);
+    };
+  }, [count]);
+
+  return null;
 }
---
+++ react-query/pages/a/index.tsx
@@ -0,0 +1,41 @@
+// pages/index.js
+import axios from "axios";
+import { UserInfoType } from "..";
+
+export async function getStaticProps() {
+  // 빌드 시점에 데이터 가져오기
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`);
+  const data = response.data as UserInfoType;
+  const result = [
+    {
+      userId: 5,
+      id: 5,
+      title: "string",
+      completed: true,
+    },
+    data,
+  ];
+
+  return {
+    props: {
+      result,
+    },
+  };
+}
+
+export default function Test({ result }: any) {
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+      <ul>
+        {result?.map((item: any) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

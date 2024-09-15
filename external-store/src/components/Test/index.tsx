4d99fc36fb57354936442955f8e55fe3f91fd75b
wo-o29 <woogur29@gmail.com>
2024-09-15 22:48:58 +0900

    feat: add Test Components

---
+++ external-store/src/components/Test/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+  return <div>테스트 컴포넌트</div>;
+}
+
+export default Test;
--- external-store/src/pages/index.tsx
+++ external-store/src/pages/index.tsx
@@ -1,16 +1,18 @@
 import countStore from "@/store";
 import { useSyncExternalStore } from "react";
+import Test from "@/components/Test";
 
-export default function App() {
-  const count = useSyncExternalStore(countStore.subscribe, countStore.read);
+export default function Home() {
+  const count = useSyncExternalStore(countStore.subscribe, countStore.read, countStore.getServerSnapshot);
 
   return (
-    <div className="App">
+    <div>
       count: {count}
       <div>
         <button onClick={countStore.increment}>Increment</button>
         <button onClick={countStore.decrement}>Decrement</button>
       </div>
+      <Test />
     </div>
   );
 }

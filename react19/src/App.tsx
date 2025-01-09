e98a05923e9530a027fe35be19c393c6abc02a53
wo-o29 <woogur29@gmail.com>
2025-01-09 10:37:17 +0900

    refactor: 안쓰는 코드 주석 처리

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Component({ id }: { id: string }) {
   const [state, setState] = useState(0);
-  console.log(`${id}: ${state}`);
+  // console.log(`${id}: ${state}`);
 
   return (
     <div>
@@ -15,19 +15,16 @@ function Component({ id }: { id: string }) {
 }
 
 function App() {
-  const [isShow, setIsShow] = useState(true);
-  if (isShow) {
-    const [state, setState] = useState(0);
-  }
+  // const [isShow, setIsShow] = useState(true);
 
   return (
     <>
       <Component id="1" />
       <Component id="2" />
       <Component id="3" />
-      <button type="button" onClick={() => setIsShow((prev) => !prev)}>
+      {/* <button type="button" onClick={() => setIsShow((prev) => !prev)}>
         클릭
-      </button>
+      </button> */}
     </>
   );
 }

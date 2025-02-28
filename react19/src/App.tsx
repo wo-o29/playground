a39e8bd2a2f01b32cd86ca6ac5a4057ab5e7f0fc
wo-o29 <woogur29@gmail.com>
2025-02-28 11:15:53 +0900

    refactor: 안쓰는 컴포넌트 제거

--- react19/src/App.tsx
+++ react19/src/App.tsx
@@ -20,7 +20,6 @@ function App() {
     <>
       <Component id="1" />
       {isShow && <Component id="2" />}
-      <Component id="3" />
       <button type="button" onClick={() => setIsShow((prev) => !prev)}>
         클릭
       </button>
